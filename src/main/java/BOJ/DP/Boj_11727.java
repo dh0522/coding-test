9a205118bb6908e615b9cf18cf3b326cc8866a64
dh0522 <gong4857@naver.com>
2023-12-23 22:38:36 +0900

    2xN 타일링2 (실버3)

---
+++ src/main/java/BOJ/DP/Boj_11727.java
@@ -0,0 +1,23 @@
+package BOJ.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_11727 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt( br.readLine() );
+        int[] d = new int[1001];
+
+        d[1] = 1;
+        d[2] = 3;
+
+        for( int i = 3; i<= n ; i++ ){
+            d[i] = (d[i-1] + 2*d[i-2])%10007;
+        }
+        System.out.println(d[n]);
+    }
+}
