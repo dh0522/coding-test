08e88ae4cdd184dc265bacf3a18f800b8a5f63a3
dh0522 <gong4857@naver.com>
2024-04-07 17:25:10 +0900

    주사위 굴리기 (골드4)

--- src/main/java/SamsungSWTest/BOJ_14499.java
+++ src/main/java/SamsungSWTest/BOJ_14499.java
@@ -1,111 +1,119 @@
 package SamsungSWTest;
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class BOJ_14499 {
-    static int n,m,x,y,k;
-    static int[][] map ;
+    // 동 1 , 서 2 , 북 3 , 남 4
+
+    static int n, m;
+
     static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0}; // 동서북남
-    static int[] dice = new int[7];
-    // 위 아래 앞면 뒷면 좌 우
+    static int[] dy = {1,-1,0,0};
+    static int[] dice = new int[6];
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br =  new BufferedReader( new InputStreamReader( System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());// 지도 크기 N by M
-        x = Integer.parseInt(st.nextToken()); // 주사위 위치
-        y = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken()); // k = 명령의 개수
+        m = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
 
-        map = new int[n][m];
+        int[][] map = new int[n][m];
 
-        for( int i=0 ;i<n; i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j= 0; j<m;j++)
+            for (int j = 0; j < m; j ++ ){
                 map[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-
 
         st = new StringTokenizer(br.readLine());
-        for( int i=0; i< k; i++){
-            int dir = Integer.parseInt(st.nextToken()) -1 ;
-            movemap(dir);
-        }
 
-        // 이동한 칸 이 0 이면 그 칸은 주사위의 바닥면에 쓰인 수가 됨
-        // 0 이 아니면 칸에 쓰여있는 수가 주사위의 바닥면이 됨 . 주사위의 칸은 0 이됨
-        // 이동 -> 이동한 칸 수 x 확인 -> 주사위의 바닥면 = x -> 지도의 칸 = 0
+        // 이동한 칸에 쓰여있는 수 = 0 -> 주사위의 바닥면에 쓰여있는 수가 칸에 복사된다.
+        // 이동한 칸이 0이 아닌 경우 -> 칸에 쓰여있는 수가 주사위의 바닥면에 복사되고 칸은 0이된다.
+        // 주사위가 이동했을때마다 상단에 쓰여 있는 값을 출력해라
 
-    }
-    static void movemap(int d){
+        for (int i = 0; i < k; i++) {
 
-        // 남북이면 dicedy
+            int dir = Integer.parseInt(st.nextToken());
 
-        int nx = x + dx[d];
-        int ny = y + dy[d];
+            int nx = x + dx[dir-1];
+            int ny = y + dy[dir-1];
 
-        if( nx<0 || ny <0 || nx>=n || ny>= m ) {
-            return;
-        }
-        movedice(d);
-        System.out.print(dice[0] +" ");
-        x = nx;
-        y = ny;
+            if( nx < 0 || ny < 0 || nx >= n || ny >= m )
+                continue;
 
-        if(map[nx][ny] == 0 )
-            map[nx][ny] = dice[1];
-        else {
-            dice[1] = map[nx][ny];
-            map[nx][ny] = 0;
+            godir( dir );
+
+            if( map[nx][ny] == 0 ){
+                map[nx][ny] = dice[1];
+            }else {
+                dice[1] = map[nx][ny];
+                map[nx][ny] = 0;
+            }
+            System.out.println( dice[0] );
+
+            x = nx;
+            y = ny;
+
         }
 
     }
-    static void movedice( int dir ){
+    public static void godir( int dir ){
+        // 위 = 0 아래 = 1 앞=2 뒤=3 왼=4 오=5
+        if( dir == 1 ){
 
-        // 동 0 서 1 북 2 남 3
-        // 위 아래 앞면 뒷면 좌 우
-        if( dir == 0 ){ // 동쪽
-            int tmp = dice[0];
-            dice[0] = dice[4];
-            dice[4] = dice[1];
+            int temp = dice[1];
             dice[1] = dice[5];
-            dice[5] = tmp;
+            dice[5] = dice[0];
+            dice[0] = dice[4];
+            dice[4] = temp;
 
+        }else if( dir == 2 ){
 
-        }
-        else if( dir == 1) { // 서쪽
-            int tmp = dice[0];
-            dice[0] = dice[5];
-            dice[5] = dice[1];
+            int temp = dice[1];
             dice[1] = dice[4];
-            dice[4] = tmp;
+            dice[4] = dice[0];
+            dice[0] = dice[5];
+            dice[5] = temp;
 
+        }else if( dir == 3){
 
-        }
-        else if( dir == 2){
-
-            int tmp = dice[0];
-            dice[0] = dice[2];
-            dice[2] =dice[1];
+            int temp = dice[1];
             dice[1] = dice[3];
-            dice[3] = tmp;
+            dice[3] = dice[0];
+            dice[0] = dice[2];
+            dice[2] = temp;
 
+        }else if ( dir == 4 ) {
 
-        }
-        else{
-            int tmp = dice[0];
-            dice[0] = dice[3];
-            dice[3] =dice[1];
+            int temp = dice[1];
             dice[1] = dice[2];
-            dice[2] = tmp;
+            dice[2] = dice[0];
+            dice[0] = dice[3];
+            dice[3] = temp;
         }
-
 
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
