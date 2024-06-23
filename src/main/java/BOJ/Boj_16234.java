d8a4088a9bbc4340d688d7694da954b7d6e7eddd
dh0522 <gong4857@naver.com>
2024-06-23 17:26:24 +0900

    인구이동 (골드4) - 다시

--- src/main/java/BOJ/Boj_16234.java
+++ src/main/java/BOJ/Boj_16234.java
@@ -2,24 +2,24 @@ package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Boj_16234 {
+	static int ans = 0;
+	static int n,l,r;
 	static int[][] map;
+	static int[] dx = {0,1,0,-1};
+	static int[] dy = {1,0,-1,0};
 	static boolean[][] visited;
-	static int sum = 0;
-	static int n, l, r;
-	static int[] dx = {1, 0, -1, 0};
-	static int[] dy = {0, 1, 0,  -1};
 	static ArrayList<int[]> list ;
-
+	static int sum = 0;
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer( br.readLine());
 
 		n = Integer.parseInt(st.nextToken());
 		l = Integer.parseInt(st.nextToken());
@@ -27,53 +27,52 @@ public class Boj_16234 {
 
 		map = new int[n][n];
 
-		for (int i = 0; i < n; i++) {
+
+		for( int i =0 ; i < n ; i++ ){
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++) {
+			for( int j = 0 ;j < n; j++ ){
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
-
 		}
-		int answer = move();
-		System.out.println(answer);
+
+		move();
+		System.out.println(ans);
 	}
+	public static void move(){
 
-
-	public static int move(){
-
-		int answer = 0;
 		while( true ){
 
-			boolean ok = false;
 			visited = new boolean[n][n];
+			boolean ok = false;
 
-			for( int i =0 ; i< n; i ++ ){
-				for( int j = 0 ; j <n ;j ++ ){
+			for( int i = 0 ; i< n; i++ ){
+				for( int j = 0; j< n; j++ ){
+
 					if( !visited[i][j] ){
 						sum = 0;
-						if( open(i,j) ) {
-							changePeople();
+						if( possible(i,j) ) {
 							ok = true;
+							letsmove();
 						}
 					}
 				}
 			}
-
 			if( !ok )
-				return answer;
-			answer++;
+				break;
+			ans++;
+
 		}
 	}
-
-	public static boolean open( int x ,int y ){
+	public static boolean possible(int x , int y ){
 
 		Queue<int[]> q = new LinkedList<>();
 		list = new ArrayList<>();
 
-		q.add( new int[] { x, y } );
 		visited[x][y] = true;
-		list.add(new int[]{x,y});
-		sum += map[x][y];
+
+		sum = map[x][y];
+		q.add( new int[]{x,y});
+		list.add( new int[]{x,y});
 
 		while( !q.isEmpty() ){
 
@@ -82,21 +81,25 @@ public class Boj_16234 {
 			x = now[0];
 			y = now[1];
 
-			for( int i =0 ;i <4; i++ ){
+			for( int i =0 ; i< 4; i++ ){
 
 				int nx = x + dx[i];
 				int ny = y + dy[i];
 
-				if( nx <0 || ny <0 || nx >=n || ny >= n )
+				if( nx < 0 || ny < 0 || nx >=n || ny >=n )
 					continue;
 
-				int diff = Math.abs( map[x][y] - map[nx][ny] );
-				if( l <= diff && diff <= r && !visited[nx][ny] ){
+				if ( visited[nx][ny] )
+					continue;
 
-					visited[nx][ny] = true;
-					q.add(new int[]{nx,ny});
-					list.add(new int[]{nx,ny});
+				int diff = Math.abs( map[nx][ny] - map[x][y] );
+
+				if( l <= diff && diff <= r ){
+
 					sum += map[nx][ny];
+					visited[nx][ny] = true;
+					q.add( new int[]{nx,ny});
+					list.add( new int[]{nx,ny});
 
 				}
 
@@ -104,31 +107,21 @@ public class Boj_16234 {
 
 		}
 
-		if( list.size() > 1 )
+		if( list.size() > 1  )
 			return true;
 		return false;
-
 	}
-	public static void changePeople() {
+	public static void letsmove(){
 
-		int num = sum / list.size();
+		int newNum = sum / list.size();
 
-		for( int i = 0 ;i < list.size(); i++ ){
-			int[] now = list.get(i);
-			int x = now[0];
-			int y = now[1];
+		for( int i =0; i< list.size(); i++ ){
 
-			map[x][y] = num;
+			int x = list.get(i)[0];
+			int y = list.get(i)[1];
+
+			map[x][y] = newNum;
+
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
