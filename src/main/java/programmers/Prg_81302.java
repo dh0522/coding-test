765670c798135ec9a407dc7f8a379973a9d3f9da
dh0522 <gong4857@naver.com>
2024-06-29 17:47:24 +0900

    거리두기 확인하기 (Lv.2)

--- src/main/java/programmers/Prg_81302.java
+++ src/main/java/programmers/Prg_81302.java
@@ -1,7 +1,8 @@
 package programmers;
 
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Queue;
 
 public class Prg_81302 {
 	public static void main(String[] args) {
@@ -9,107 +10,73 @@ public class Prg_81302 {
 		String[][] places = {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
 			{"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
 		int[] answer = sol.solution( places );
-		for( int i : answer )
-			System.out.print( i +" ");
-		System.out.println();
+		System.out.println(Arrays.toString(answer));
 	}
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	static int[] mandx = {1,1,-1,-1};
-	static int[] mandy = {1,-1,-1,1};
-	static char[][] seat;
-	public static int[] solution( String[][] places ){
-
+	public static int[] solution(String[][] places){
 		int[] answer = new int[5];
-		seat = new char[5][5];
-
-		for( int i = 0; i< 5 ; i++ ){
-			for( int j =0 ; j < 5; j++ )
-				seat[j] = places[i][j].toCharArray();
-
-			int result = correct();
-			answer[i] = result;
-
+		for( int i=0; i < 5; i++ ){
+			answer[i] = search(places[i]);
 		}
-
 		return answer;
 	}
-	private static int correct(){
 
-		int answer = 1;
+	private static int search( String[] place ){
 
-		List<int[]> list = new LinkedList<>();
+		char[][] seat = new char[5][5];
+		for( int i =0 ;i < 5; i++ ){
+			seat[i] = place[i].toCharArray();
+		}
 
-		for( int i = 0 ; i < 5; i ++ ){
-			for( int j = 0; j< 5; j++ ){
+		for( int i =0 ;i < 5; i++ ){
+			for( int j =0 ;j < 5; j++ ){
+
 				if( seat[i][j] == 'P' ){
-					list.add( new int[] {i,j} );
+					if( !isPossible(i,j,seat) ){
+						return 0;
+					}
 				}
 			}
 		}
 
-		for( int i = 0 ; i < list.size() ; i ++ ){
-			int[] person = list.get(i);
-
-			if( !next(person) ){
-				answer = 0;
-				break;
-			}
-
-			// 맨허턴 거리에 있는지 판단하기
-			if( !manhattan(person) ){
-				answer = 0;
-				break;
-			}
-		}
-		return answer;
-
+		return 1;
 	}
-	private static boolean next( int[] person ){
+	private static boolean isPossible( int x , int y , char[][] seat ){
 
+		int[] dx = {1,0,-1,0};
+		int[] dy = {0,1,0,-1};
+		boolean[][] visited = new boolean[5][5];
 
-		for( int i = 0 ; i< 4; i++ ){
-			int nx = person[0] + dx[i];
-			int ny = person[1] + dy[i];
+		Queue<int[]> q = new LinkedList<>();
+		q.add( new int[]{x,y} );
+		visited[x][y] = true;
 
-			if ( nx < 0 || ny < 0 || nx >= 5 || ny >= 5 )
-				continue;
+		while( !q.isEmpty() ){
+			int[] now = q.poll();
 
-			if( seat[nx][ny] == 'P' )
-				return false;
-			else if(seat[nx][ny] == 'O') {
+			for( int i =0 ; i < 4; i++ ){
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
 
-				nx += dx[i];
-				ny += dy[i];
-
-				if ( nx < 0 || ny < 0 || nx >= 5 || ny >= 5 )
+				if( nx < 0 || ny < 0 || nx >= 5 || ny >= 5 )
 					continue;
 
-				if( seat[nx][ny] == 'P' )
-					return false;
-			}
-		}
-		return true;
-	}
-	public static boolean manhattan( int[] person ){
+				if( visited[nx][ny] || seat[nx][ny] == 'X' )
+					continue;
 
-		for( int i = 0 ; i< 4; i++ ){
-			int nx = person[0] + mandx[i];
-			int ny = person[1] + mandy[i];
+				if( Math.abs(x-nx)+ Math.abs(y-ny) > 2 )
+					continue;
 
-			if ( nx < 0 || ny < 0 || nx >= 5 || ny >= 5 )
-				continue;
-
-			if( seat[nx][ny] == 'P' ){
-
-				if( seat[nx][person[1]] == 'O')
-					return false;
-				if( seat[person[0]][ny] == 'O'){
+				if( seat[nx][ny] == 'P' ){
 					return false;
 				}
+
+				q.add( new int[]{nx,ny});
+				visited[nx][ny] = true;
+
 			}
 		}
-		return true;
-	}
 
+		return true;
+
+	}
 }
