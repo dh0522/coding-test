21d2bb6058107ca2a85f4bb5df9888543b6c13a9
dh0522 <gong4857@naver.com>
2024-07-07 14:04:31 +0900

    인구 이동 (골드4)

--- src/main/java/BOJ/Boj_16234.java
+++ src/main/java/BOJ/Boj_16234.java
@@ -3,125 +3,95 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Boj_16234 {
-	static int ans = 0;
-	static int n,l,r;
-	static int[][] map;
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
-	static boolean[][] visited;
-	static ArrayList<int[]> list ;
-	static int sum = 0;
+	static int[][] country;
+	static int n, l, r;
+	static int[] dx = {1,0,-1,0};
+	static int[] dy = {0,1,0,-1};
+	static boolean[][] possible;
 	public static void main(String[] args) throws Exception {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer( br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		n = Integer.parseInt(st.nextToken());
 		l = Integer.parseInt(st.nextToken());
 		r = Integer.parseInt(st.nextToken());
 
-		map = new int[n][n];
-
+		country = new int[n][n];
 
 		for( int i =0 ; i < n ; i++ ){
 			st = new StringTokenizer(br.readLine());
-			for( int j = 0 ;j < n; j++ ){
-				map[i][j] = Integer.parseInt(st.nextToken());
+			for( int j =0 ;j < n; j++ ){
+				country[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		move();
-		System.out.println(ans);
-	}
-	public static void move(){
-
+		int answer = 0;
 		while( true ){
+			int num = 0;
+			possible = new boolean[n][n];
 
-			visited = new boolean[n][n];
-			boolean ok = false;
-
-			for( int i = 0 ; i< n; i++ ){
-				for( int j = 0; j< n; j++ ){
-
-					if( !visited[i][j] ){
-						sum = 0;
-						if( possible(i,j) ) {
-							ok = true;
-							letsmove();
-						}
+			for( int i = 0 ; i < n; i++ ){
+				for( int j = 0 ;j < n ; j++ ){
+					if( !possible[i][j] ) {
+						num += isOpen(i, j);
 					}
 				}
 			}
-			if( !ok )
+
+			if( num == 0 )
 				break;
-			ans++;
+			answer++;
 
 		}
+		System.out.println(answer);
 	}
-	public static boolean possible(int x , int y ){
+	public static int isOpen(int x , int y){
 
 		Queue<int[]> q = new LinkedList<>();
-		list = new ArrayList<>();
+		List<int[]> list = new ArrayList<>();
 
-		visited[x][y] = true;
-
-		sum = map[x][y];
-		q.add( new int[]{x,y});
-		list.add( new int[]{x,y});
-
-		while( !q.isEmpty() ){
-
+		q.add(new int[]{x,y});
+		while(!q.isEmpty()){
 			int[] now = q.poll();
-
 			x = now[0];
 			y = now[1];
 
-			for( int i =0 ; i< 4; i++ ){
-
+			for( int i = 0; i < 4; i++ ){
 				int nx = x + dx[i];
 				int ny = y + dy[i];
 
-				if( nx < 0 || ny < 0 || nx >=n || ny >=n )
+				if( nx < 0 || ny < 0 || nx >=n || ny >= n )
 					continue;
 
-				if ( visited[nx][ny] )
+				if( possible[nx][ny] )
 					continue;
 
-				int diff = Math.abs( map[nx][ny] - map[x][y] );
-
+				int diff = Math.abs( country[x][y] - country[nx][ny] );
 				if( l <= diff && diff <= r ){
-
-					sum += map[nx][ny];
-					visited[nx][ny] = true;
-					q.add( new int[]{nx,ny});
-					list.add( new int[]{nx,ny});
-
+					possible[nx][ny] = true;
+					q.add(new int[]{nx,ny});
+					list.add(new int[]{nx,ny});
 				}
-
 			}
+		}
+		int allPeople = 0;
 
+		// 국경선 열고 인구 나누기
+		for( int i =0 ; i <list.size(); i++ ){
+			int[] nara = list.get(i);
+			allPeople += country[nara[0]][nara[1]];
 		}
 
-		if( list.size() > 1  )
-			return true;
-		return false;
-	}
-	public static void letsmove(){
-
-		int newNum = sum / list.size();
-
-		for( int i =0; i< list.size(); i++ ){
-
-			int x = list.get(i)[0];
-			int y = list.get(i)[1];
-
-			map[x][y] = newNum;
-
+		for (int i = 0 ; i < list.size(); i++ ){
+			int[] nara= list.get(i);
+			country[nara[0]][nara[1]] = allPeople/list.size();
 		}
+
+		return list.size();
 	}
 }
