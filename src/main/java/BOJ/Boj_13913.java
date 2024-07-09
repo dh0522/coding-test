d49bf163dd373bf5257a758b71a8d18a6b199fca
dh0522 <gong4857@naver.com>
2024-07-09 14:45:29 +0900

    숨바꼭질 4 (골드4)

--- src/main/java/BOJ/Boj_13913.java
+++ src/main/java/BOJ/Boj_13913.java
@@ -3,76 +3,63 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Boj_13913 {
-	static int answer = Integer.MAX_VALUE;
-	static int[] visited;
-	public static void main(String[] args) throws  Exception {
-
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 
-		visited = new int[100001];
-		bfs( n , k );
-
-
-	}
-	private static void bfs( int n , int k ){
-
-		Queue<Integer> q = new LinkedList<>();
-		q.add(n);
+		int[] visited = new int[100001];
 		int[] parent = new int[100001];
 
 		visited[n] = 1;
+		Queue<Integer> q = new LinkedList<>();
+		q.add( n );
+		int ans = 0;
 
 		while( !q.isEmpty() ){
-			int now = q.poll();
 
+			int now = q.poll();
 			if( now == k ){
-				answer = visited[now] -1;
+				ans = visited[now] -1 ;
 				break;
 			}
-
-			for( int i = 0 ; i< 3; i ++ ){
+			for( int i = 0 ; i < 3; i++ ){
 				int next;
+				if( i == 0 ){
+					next = now + 1;
+				}else if( i == 1){
+					next = now -1;
+				}else next = now*2;
 
-				if( i == 0 )
-					next = now *2;
-				else if( i==1 )
-					next = now +1;
-				else next = now-1;
-
-				if( next < 0 || next >= 100001 || visited[next] != 0 )
+				if( next < 0 || next >= visited.length || visited[next] != 0 )
 					continue;
 
-				q.add(next);
-				visited[next] = visited[now] +1;
 				parent[next] = now;
+				q.add(next);
+				visited[next] = visited[now] + 1;
 			}
 		}
 
-		System.out.println(answer);
-		int idx = k;
-		Stack<String> stk = new Stack<>();
-
-		while( idx != n ){
-
-			stk.push( String.valueOf(idx) );
-			idx = parent[idx];
+		StringBuilder sb = new StringBuilder();
+		sb.append(ans+"\n");
+		List<Integer> list = new ArrayList<>();
+		while( k != n ){
+			list.add(k);
+			k = parent[k];
 		}
-		stk.push(String.valueOf(n));
-
-		while( !stk.isEmpty()) {
-			System.out.print(stk.pop()+" ");
+		sb.append(n+" ");
+		for( int i = list.size()-1 ; i >=0 ; i-- ){
+			sb.append(list.get(i) +" ");
 		}
-
-
+		System.out.println(sb.toString());
 
 	}
 }
