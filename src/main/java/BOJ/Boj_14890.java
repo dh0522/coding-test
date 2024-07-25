a56d02d19c3a0440686d766b13cf747fc2389a3f
dh0522 <gong4857@naver.com>
2024-07-25 17:35:24 +0900

    경사로(골드3)-다시

--- src/main/java/BOJ/Boj_14890.java
+++ src/main/java/BOJ/Boj_14890.java
@@ -5,11 +5,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Boj_14890 {
-	static int[][] arr;
-	static int answer;
 	static int n,l;
+	static int[][] arr;
+	static int answer = 0;
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		n = Integer.parseInt(st.nextToken());
@@ -17,107 +17,110 @@ public class Boj_14890 {
 
 		arr = new int[n][n];
 
-		for( int i =0 ; i <n ;i++ ){
+
+		for( int i =0; i <n ; i++ ){
 			st = new StringTokenizer(br.readLine());
-			for( int j =0; j< n ;j++ ){
+			for( int j= 0; j < n; j ++ )
 				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
 		}
 
-		for( int i =0; i< n ;i++ ){
-
-			isRow( i );
+		for( int i = 0; i< n ; i++){
+			isRow(i);
 			isColumn(i);
-		}
 
+		}
 		System.out.println(answer);
+
 	}
 	private static void isColumn( int column ){
-
 		boolean[] put = new boolean[n];
 
-		for( int i =0; i < n-1;i ++ ) {
-			int diff = arr[i+1][column] - arr[i][column] ;
+		for( int i = 0; i <n-1; i++ ){
+
+			int diff = arr[i+1][column] - arr[i][column];
+			if( diff == 0 )
+				continue;
 			if( Math.abs(diff) >= 2 )
 				return;
-			if( diff == 0 ) {
-				continue;
-			}
-			// i < i+1 : 다음 것이 더 큼
-			else if( diff == 1 ){
+
+			if( diff == 1 ){
 
 				for( int j = i; j > i-l; j-- ){
 					if( j < 0 || j >= n )
 						return;
-					if( arr[i][column] != arr[j][column] )
-						return;
 					if( put[j] )
 						return;
+					if( arr[j][column] != arr[i][column] )
+						return;
+
 					put[j] = true;
 				}
 
 			}else if( diff == -1 ){
-				for( int j = i+1; j <= i + l; j++ ){
+
+				for( int j = i+1; j <= i + l ; j++ ){
 					if( j < 0 || j >= n )
-						return;
-					if( arr[i+1][column] != arr[j][column] )
 						return;
 					if( put[j] )
 						return;
+					if( arr[j][column] != arr[i+1][column] )
+						return;
+
 					put[j] = true;
 				}
+				i =i+l-1;
 
-				i = i+l-1;
 			}
 
 		}
-
 		answer++;
-
 
 	}
-	private static void isRow(int row){
+	private static void isRow( int row ){
 
 		boolean[] put = new boolean[n];
 
-		for( int i =0; i < n-1;i ++ ) {
-			int diff = arr[row][i+1] - arr[row][i] ;
+		for( int i =0; i< n-1; i++ ){
+
+			int diff = arr[row][i+1] - arr[row][i];
+
+			if( diff == 0 )
+				continue;
 			if( Math.abs(diff) >= 2 )
 				return;
-			if( diff == 0 ) {
-				continue;
-			}
-			// i < i+1 : 다음 것이 더 큼
-			else if( diff == 1 ){
 
-				for( int j = i; j > i-l; j-- ){
+			// i <  i+1
+			if( diff == 1 ){
+
+				for( int j = i; j > i-l ; j --){
 					if( j < 0 || j >= n )
 						return;
-					if( arr[row][i] != arr[row][j] )
-						return;
 					if( put[j] )
+						return;
+					if( arr[row][j] != arr[row][i] )
 						return;
 					put[j] = true;
 				}
 
-			}else if( diff == -1 ){
-				for( int j = i+1; j <= i + l; j++ ){
+			}
+			else if( diff == -1 ){ // i > i+1
+
+				for( int j = i+1; j <= i +l; j++ ){
+
 					if( j < 0 || j >= n )
-						return;
-					if( arr[row][i+1] != arr[row][j] )
 						return;
 					if( put[j] )
 						return;
+					if( arr[row][j] != arr[row][i+1] )
+						return;
+
 					put[j] = true;
+
 				}
-
-				i = i+l-1;
+				i = i + l-1;
 			}
-
 		}
 
 		answer++;
-
-
 	}
 }
