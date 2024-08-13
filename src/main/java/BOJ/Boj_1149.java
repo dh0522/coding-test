14b14805e0ba585409e561e4dde0bc47d765d70e
dh0522 <gong4857@naver.com>
2024-08-13 19:13:16 +0900

    RGB 거리(실버1)

--- src/main/java/BOJ/Boj_1149.java
+++ src/main/java/BOJ/Boj_1149.java
@@ -6,41 +6,28 @@ import java.util.StringTokenizer;
 
 public class Boj_1149 {
 	public static void main(String[] args) throws Exception {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][3];
 
-		int[][] cost = new int[n+1][3];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		StringTokenizer st;
-		for( int i = 1 ; i <= n ; i++ ){
+		arr[0][0] = Integer.parseInt(st.nextToken());
+		arr[0][1] = Integer.parseInt(st.nextToken());
+		arr[0][2] = Integer.parseInt(st.nextToken());
+
+		for( int i=1; i < n ;i++ ){
 			st = new StringTokenizer(br.readLine());
+
 			int r = Integer.parseInt(st.nextToken());
 			int g = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 
-			cost[i][0] = Math.min( cost[i-1][1] , cost[i-1][2] ) + r;
-			cost[i][1] = Math.min( cost[i-1][0] , cost[i-1][2] ) + g;
-			cost[i][2] = Math.min( cost[i-1][0] , cost[i-1][1] ) + b;
-
+			arr[i][0] = Math.min( arr[i-1][1] + r , arr[i-1][2] + r );
+			arr[i][1] = Math.min( arr[i-1][0] + g , arr[i-1][2] + g );
+			arr[i][2] = Math.min( arr[i-1][1] + b , arr[i-1][0] + b );
 		}
 
-		System.out.println(Math.min(Math.min( cost[n][0] ,cost[n][1]),cost[n][2]));
-
-
+		System.out.println(Math.min(arr[n-1][0] , Math.min(arr[n-1][1], arr[n-1][2] )) );
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
