7b40a95098ec0cfe3060bfc6250b9057de5d0736
dh0522 <gong4857@naver.com>
2024-08-18 14:42:32 +0900

    테트로미노 (골드4)

--- src/main/java/BOJ/Boj_14500.java
+++ src/main/java/BOJ/Boj_14500.java
@@ -5,69 +5,67 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Boj_14500 {
+	static int[][] arr;
+	static int answer = Integer.MIN_VALUE;
 	static int[] dx = {1,0,-1,0};
 	static int[] dy = {0,1,0,-1};
-	static int[][] arr;
 	static boolean[][] visited;
-	static int answer = 0;
+	static int n,m;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
 		arr = new int[n][m];
 		visited = new boolean[n][m];
 
-		for( int i = 0 ; i < n ; i++ ){
-			st  = new StringTokenizer(br.readLine());
-			for( int j =0  ; j < m; j ++ ){
+		for( int i =0; i < n; i++ ){
+			st = new StringTokenizer(br.readLine());
+			for( int j=0; j < m; j++ )
 				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
 
-		for( int i = 0 ; i < arr.length; i++ ){
-			for( int j = 0; j < arr[0].length ; j++ ){
+		}
+		for( int i=0; i < n; i++ ){
+			for( int j=0; j < m; j ++ ) {
 				visited[i][j] = true;
-				dfs(  1 , i, j ,arr[i][j]  );
+				searchMax(i, j, 1, arr[i][j]);
 				visited[i][j] = false;
-
 			}
-		}
 
+		}
 		System.out.println(answer);
 	}
+	private static void searchMax( int x , int y , int depth , int sum ){
 
-	private static void dfs( int count , int x, int y ,int sum ){
-
-		if( count == 4 ){
+		if( depth == 4 ){
 			answer = Math.max( answer , sum );
 			return;
 		}
 
-		for( int i = 0; i< 4; i++ ){
+		for( int i =0; i < 4; i++ ){
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 
-			if( nx < 0 || ny < 0 || nx >= arr.length || ny >= arr[0].length )
+			if( nx < 0 || ny < 0 || nx >= n || ny >= m )
 				continue;
 
 			if( visited[nx][ny] )
 				continue;
 
-			if( count == 2 ){
+			if( depth == 2 ){
+
 				visited[nx][ny] = true;
-				dfs(count+1, x, y,sum+arr[nx][ny] );
+				searchMax(x,y,depth+1,sum+arr[nx][ny]);
 				visited[nx][ny] = false;
+
 			}
 
 			visited[nx][ny] = true;
-			dfs( count+1, nx,ny , sum + arr[nx][ny] );
+			searchMax(nx,ny,depth+1,sum+arr[nx][ny]);
 			visited[nx][ny] = false;
-
 		}
 
 	}
-
 }
