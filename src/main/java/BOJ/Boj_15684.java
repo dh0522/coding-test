b5c644b4863122db9feaa4e5a7ffef082c76a66b
dh0522 <gong4857@naver.com>
2024-08-29 17:05:28 +0900

    사다리 조작(골드3)-다시

--- src/main/java/BOJ/Boj_15684.java
+++ src/main/java/BOJ/Boj_15684.java
@@ -6,102 +6,97 @@ import java.util.StringTokenizer;
 
 public class Boj_15684 {
 	static int n,m,h;
-	static boolean possible;
-	static int[][] line;
+	static int[][] arr;
+	static boolean poss = false;
+	static int answer = -1;
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		h = Integer.parseInt(st.nextToken());
 
-		line = new int[h][n];
+		arr = new int[h][n];
 
-		for( int i=0; i< m; i++ ){
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken()) -1;
-			int b = Integer.parseInt(st.nextToken()) -1;
+		while ( m-- >0 ){
+			st = new StringTokenizer( br.readLine() );
+			int a = Integer.parseInt(st.nextToken())-1;
+			int b = Integer.parseInt(st.nextToken())-1;
 
-			/// n == b번 세로선 b+1번 세로선 연결
-			// h == a번 점선위치에서 연결
-
-			line[a][b] = 1;
-			line[a][b+1] = 2;
+			arr[a][b] = 2;
+			arr[a][b+1] = -2;
 		}
 
-		int answer = -1;
-
-		for( int i =0; i< 4; i++ ){
-
-			dfs( i, 0,0);
-
-			if( possible ){
+		for( int i=0 ; i< 4; i++ ){
+			dfs( i , 0, 0 );
+			if( poss ) {
 				answer = i;
 				break;
 			}
 		}
-		System.out.println(answer);
 
+		System.out.println( answer );
 	}
+	private static void dfs( int max , int start , int depth ){
 
-	/**
-	 * 	점선을 새로 연결하기
-	 */
-	private static void dfs( int newNum , int x , int depth ){
-
-		if( newNum == depth ) {
-			if( isRight() )
-				possible = true;
+		if( max  == depth ){
+			isAll();
 			return;
 		}
 
+		for( int i = start; i < h ;i ++ ){
+			for( int j = 0; j < n-1 ; j++ ){
+				if( arr[i][j] == 0 && arr[i][j+1] == 0 ) {
 
-		for( int i = x ; i < h ;i++ ){
+					arr[i][j] = 2;
+					arr[i][j + 1] = -2;
 
-			for( int j = 0 ; j < n-1  ; j++ ){
+					dfs(max, i, depth + 1);
 
-				if( line[i][j] == 0 && line[i][j+1] == 0 ) {
-
-					line[i][j] = 1;
-					line[i][j + 1] = 2;
-					dfs( newNum , i, depth + 1);
-					line[i][j] = 0;
-					line[i][j + 1] = 0;
+					arr[i][j] = 0;
+					arr[i][j + 1] = 0;
 				}
 			}
 		}
 
 	}
-	/**
-	 *   i 가 i 로 도착하는지 확인
-	 *   => true : 모두 잘 도착
-	 *   => false : 아님
-	 */
-	private static boolean isRight(){
+	private static void isAll(){
 
-		for( int i =0; i< n; i++ ){
+		for( int i = 0; i < n; i++ ){
+			int start = i;// n번째 세로선
+			int garo = 0; // 가로는 항상 0부터 시작
 
-			int x = 0; // 높이가 0 => 시작점
-			int y = i; // 세로선 n을 의미함
-
-			for( int j = 0; j < h; j++ ){
-
-				if( line[x][y] == 1 ){
-					y++;
-				}else if( line[x][y] == 2 ){
-					y--;
+			for( int j =0 ; j < h; j++) {
+				if( arr[garo][start] == 2 ){
+					start++;
+				}else if( arr[garo][start] == -2 ){
+					start--;
 				}
-				x++;
-
+				garo++;
 			}
 
-			if( y != i )
-				return false;
-
+			if( start != i )
+				return;
 
 		}
-
-		return true;
+		poss = true;
 	}
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
