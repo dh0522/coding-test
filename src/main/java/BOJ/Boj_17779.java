f13bd9e97b725a742b1bb0c693e78e2fac2f59a9
dh0522 <gong4857@naver.com>
2024-08-31 16:25:37 +0900

    게리맨더링2 (골드3) -다시 ;

--- src/main/java/BOJ/Boj_17779.java
+++ src/main/java/BOJ/Boj_17779.java
@@ -2,113 +2,94 @@ package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Boj_17779 {
+	static int[][] arr;
 	static int n;
-	static int[] people;
-	static boolean[] visited;
 	static int answer = Integer.MAX_VALUE;
-	static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+	static int allNumber = 0;
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt( br.readLine());
-		people = new int[n+1];
-		visited = new boolean[n+1];
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n][n];
 
-		for( int i =0; i <=n ; i++ )
-			list.add( new ArrayList<>() );
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for( int i=1; i <= n ; i++ ){
-			people[i] = Integer.parseInt(st.nextToken());
-		}
-
-		for( int i =1 ; i<= n ; i++ ){
+		StringTokenizer st;
+		for(int i =0; i <n ;i ++ ){
 			st = new StringTokenizer(br.readLine());
-			int adjPeople = Integer.parseInt(st.nextToken());
-			for( int j=0; j <adjPeople; j++ ){
-				list.get(i).add(Integer.parseInt(st.nextToken()));
+			for(int j=0; j < n; j++ ){
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				allNumber += arr[i][j];
 			}
 		}
 
-		dfs(0 );
-
-		if( answer == Integer.MAX_VALUE )
-			System.out.println(-1);
-		else
-			System.out.println(answer);
-
-	}
-	private static void dfs( int idx ){
-		if( idx == n ){
-
-			List<Integer> list1 = new ArrayList<>();
-			List<Integer> list2 = new ArrayList<>();
-
-			for( int i=1 ; i<= n; i++ ){
-				if( visited[i] )
-					list1.add( i );
-				else list2.add(i );
-			}
-
-			if( list1.isEmpty() || list2.isEmpty() )
-				return;
-
-			if( isConnect(list1) && isConnect(list2) ){
-				int sum1 = 0;
-				int sum2 = 0;
-
-				for( int i =1; i<=n ; i++ ){
-					if( visited[i] )
-						sum1 += people[i];
-					else sum2+= people[i];
+		for( int x = 0; x < n-2 ; x++ ){
+			for( int y = 1; y < n-1; y++ ){
+				for( int d1 = 1; d1 <n; d1++ ){
+					for( int d2 = 1; d2 <n; d2++ ){
+						if( x + d1 + d2 >= n || y-d1 < 0 || y+d2 >= n )
+							continue;
+						createArea(x,y,d1,d2);
+					}
 				}
-
-				answer = Math.min( answer , Math.abs(sum1 - sum2 ) );
 			}
-			return;
 		}
 
-		visited[idx] = true;
-		dfs( idx+1 );
-		visited[idx] = false;
-		dfs(idx+1 );
-
+		System.out.println(answer);
 
 	}
+	private static void createArea( int x, int y , int d1, int d2 ){
+		boolean[][] five = new boolean[n][n];
 
-	private static boolean isConnect( List<Integer> list1 ){
+		for( int i= 0 ; i <= d1; i++ ){
+			five[x+i][y-i]= true;
+			five[x+d2+i][y+d2-i] = true;
+		}
 
-		boolean[] connect = new boolean[n+1];
+		for( int i=0; i <= d2; i++ ){
+			five[x+i][y+i] = true;
+			five[x+d1+i][y-d1+i] = true;
+		}
 
-		Queue<Integer> q = new LinkedList<>();
-		q.add(list1.get(0));
-		connect[list1.get(0)] = true;
-
-		while( !q.isEmpty() ) {
-			int num = q.poll();
-
-			for( int i=0; i < list.get(num).size(); i++ ){
-				int next = list.get(num).get(i);
-				if( connect[next] )
-					continue;
-				if( !list1.contains(next) )
-					continue;
-
-				connect[next] = true;
-				q.add(next);
+		int[] sum = new int[5];
+		for (int i = 0; i < x + d1; i++) {
+			for (int j = 0; j <= y; j++) {
+				if( five[i][j] )
+					break;
+				sum[0] += arr[i][j];
 			}
 		}
-		for( int tmp : list1 ){
-			if( !connect[tmp] )
-				return false;
+
+
+		for (int i = 0; i <= x + d2; i++) {
+			for (int j = n - 1; j > y; j--) {
+				if( five[i][j] )
+					break;
+				sum[1] += arr[i][j];
+			}
 		}
-		return true;
+
+		for (int i = x + d1; i < n; i++) {
+			for (int j = 0; j < y - d1 + d2; j++) {
+				if( five[i][j] )
+					break;
+				sum[2] += arr[i][j];
+			}
+		}
+
+		for (int i = x + d2 + 1; i < n; i++) {
+			for (int j = n - 1; j >= y - d1 + d2; j--) {
+				if( five[i][j] )
+					break;
+				sum[3] += arr[i][j];
+			}
+		}
+
+		sum[4] = allNumber -( sum[0]+sum[1]+sum[2]+sum[3]);
+		Arrays.sort( sum );
+		answer = Math.min( answer , sum[4]- sum[0] );
+
 	}
 }
