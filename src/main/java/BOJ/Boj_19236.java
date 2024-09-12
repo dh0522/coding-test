b9e7652df71aae8ac674ee7df0137ceb55972267
dh0522 <gong4857@naver.com>
2024-09-12 17:19:10 +0900

    청소년 상어(골드2) -다시

--- src/main/java/BOJ/Boj_19236.java
+++ src/main/java/BOJ/Boj_19236.java
@@ -2,7 +2,6 @@ package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Boj_19236 {
@@ -15,7 +14,7 @@ public class Boj_19236 {
 		StringTokenizer st;
 
 
-		int[][] arr = new int[4][4];
+		int[][] arr = new int[4][4]; // arr[i][j] = (I,j) 칸에 있는 물고기 인덱스
 		Fish[] fish = new Fish[17];
 		for (int i=0;i < 4; i++ ){
 			st = new StringTokenizer(br.readLine());
@@ -24,55 +23,62 @@ public class Boj_19236 {
 				int index = Integer.parseInt(st.nextToken());
 				int dir = Integer.parseInt(st.nextToken())-1; // direction
 
-				Fish now = new Fish( i, j , dir , true );
-
+				fish[index] = new Fish( i, j , dir ,true );
 				arr[i][j] = index;
-				fish[index] = now;
+
 			}
 		}
 
-		dfs ( 0,0,0,0  , fish , arr );
+		dfs ( 0,0  ,0, fish , arr );
+
 		System.out.println(answer);
 
 	}
-	private static void dfs( int x , int y, int dir, int sum ,Fish[] fish,  int[][] arr ){
+	private static void dfs( int x , int y, int sum , Fish[] fish, int[][] arr){
 
-		// 상어가 물고기 먹고 dir 정하기
-		sum += arr[x][y];
-		dir = fish[arr[x][y]].dir;
-		fish[arr[x][y]].alive = false;
 
-		arr[x][y] = -100;
+		int nowFish = arr[x][y];
+		sum += nowFish;
+		int dir = fish[nowFish].dir;
+		//  shark = x,y,dir;
+
+		fish[ nowFish ].alive = false;
 
 		answer = Math.max( sum, answer );
 
 		// 물고기들 이동하기
 		for( int i =1; i<= 16; i++ ){
 			if( fish[i].alive ){
-				moveFish( i , fish , arr );
+
+				// x ,y = 상어 위치 i= 지금 움직일 물고기
+				moveFish( x, y, i , fish , arr );
 			}
 		}
 
-		// 상어 이동하기
+		// 상어 이동하기 최대 3칸 이동 가능하다.
 		for( int i =1; i <=3; i++ ){
 
 			int nx = x + dx[dir]*i;
 			int ny = y + dy[dir]*i;
 
-			if( nx< 0 || ny< 0 || nx >= 4 || ny >= 4 || arr[nx][ny] == -100 || !fish[ arr[nx][ny] ].alive )
+
+			// 범위를 넘거나 fish가 없는 칸은 안돼
+			if( nx< 0 || ny< 0 || nx >= 4 || ny >= 4 )
+				continue;
+			if( !fish[arr[nx][ny]].alive )
 				continue;
 
+			// nx, ny = 상어가 이동하기 위한 칸
 			Fish[] tmpFish = new Fish[17];
-			for( int k=0; k < 17; k++ ) {
-				tmpFish[k] = fish[k];
+			for (int j = 1; j < 17; j++) {
+					tmpFish[j] = new Fish(fish[j].x, fish[j].y, fish[j].dir, fish[j].alive );
 			}
+
 			int[][] tmpArr = new int[4][4];
 			for( int k =0; k < 4; k++ )
 				tmpArr[k] = arr[k].clone();
 
-			tmpArr[x][y] = 0;
-
-			dfs( nx, ny, dir , sum , fish , arr );
+			dfs( nx, ny, sum , fish , arr );
 
 			fish = tmpFish;
 			arr = tmpArr;
@@ -81,57 +87,49 @@ public class Boj_19236 {
 
 
 	}
-	private static void moveFish( int idx , Fish[] fish , int[][] arr ){
+	private static void moveFish( int sharkX, int sharkY, int idx , Fish[] fish , int[][] arr ){
 
-			// idx번째 물고기 이동하기
-			Fish nowFish = fish[idx];
-			int dir = nowFish.dir;
+		// idx번째 물고기 이동하기
 
+		/**
+		 *   놓치고 있던 것 = shark 의 위치 !
+		 */
 
-			for( int i =0; i < 8; i++ ){
-				 int x = nowFish.x + dx[dir];
-				 int y = nowFish.y + dy[dir];
+		Fish nowFish = fish[idx];
+		int dir = nowFish.dir;
 
-				 if( x  > 0 && y > 0 && x < 4 && y < 4 && arr[x][y] != -100 ){
+		for( int i =0 ; i < 8; i++ ){
 
-					 if( arr[x][y] == 0 ) {
+			int x = nowFish.x + dx[dir];
+			int y = nowFish.y + dy[dir];
 
-						 arr[nowFish.x][nowFish.y] = 0;
-						 arr[x][y] = idx;
+			if( x  >= 0 && y >= 0 && x < 4 && y < 4 && !( sharkX == x && sharkY == y )  ){
 
-						 Fish newFish = new Fish(x,y,dir,true);
-						 fish[idx] = newFish;
+				int next = arr[x][y];
+				arr[nowFish.x][nowFish.y] = next;
+				arr[x][y] = idx;
 
-					 }else{
+				fish[next].x = nowFish.x;
+				fish[next].y = nowFish.y;
 
-						 int next = arr[x][y];
-
-						 fish[next].x = nowFish.x;
-						 fish[next].y = nowFish.y;
-
-						 fish[idx].x = x;
-						 fish[idx].y = y;
-						 fish[idx].dir = dir;
-
-						 arr[x][y] = idx;
-						 arr[nowFish.x][nowFish.y] = next;
-
-					 }
-
-					 return;
-				 }
-
-				dir = (dir+1)%8;
+				fish[idx].x = x;
+				fish[idx].y = y;
+				fish[idx].dir = dir;
+				return;
 			}
-
-
+			dir = (dir+1)%8;
 		}
+
+	}
+
+
+
 
 	static class Fish {
 		int x, y;
 		int dir;
-		boolean alive ;
-		Fish(int x, int y, int dir , boolean alive ) {
+		boolean alive;
+		Fish(int x, int y, int dir ,boolean alive ) {
 			this.x = x;
 			this.y = y;
 			this.dir = dir;
