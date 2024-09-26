2e0f0f3bfcc1ffded763fc02d98f1052f2a3a12e
dh0522 <gong4857@naver.com>
2024-09-26 16:49:51 +0900

    로프(실버4)

--- src/main/java/BOJ/Boj_2217.java
+++ src/main/java/BOJ/Boj_2217.java
@@ -3,9 +3,6 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Boj_2217 {
 	public static void main(String[] args) throws Exception {
@@ -13,23 +10,22 @@ public class Boj_2217 {
 
 		int n = Integer.parseInt(br.readLine());
 
-		List<Integer> list = new ArrayList<>();
+
 		int[] weight = new int[n];
 		for (int i=0; i <n; i++ ){
 			weight[i] = Integer.parseInt(br.readLine());
-			list.add( weight[i] );
 		}
 
 		Arrays.sort( weight );
 
+		int answer = Integer.MIN_VALUE;
 
 		for (int i=0;i < n; i++ ){
-			int now = weight[i];
-			list.add( now*(n-i) );
+			answer = Math.max( answer , weight[i]*(n-i) );
 		}
-		Collections.sort( list );
 
-		System.out.println(list.get(list.size()-1));
+		System.out.println(answer);
+
 	}
 }
 
