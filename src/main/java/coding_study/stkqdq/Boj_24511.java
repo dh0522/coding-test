ae97f26693a303ceeda9e4d17fb4782ccc61f835
dh0522 <gong4857@naver.com>
2025-01-06 20:18:11 +0900

    queuestack(실버3)

---
+++ src/main/java/coding_study/stkqdq/Boj_24511.java
@@ -0,0 +1,47 @@
+package coding_study.stkqdq;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Boj_24511 {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] type = new int[n];
+		Deque<Integer> q = new ArrayDeque<>();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			type[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for( int i =0; i < n; i ++ ){
+			if( type[i] == 0 )
+				q.add(Integer.parseInt(st.nextToken()));
+
+			else st.nextToken();
+
+		}
+
+		int m = Integer.parseInt(br.readLine());
+		int addOne = 0;
+
+		st = new StringTokenizer(br.readLine());
+		for( int i =0; i < m; i++ ){
+			addOne = Integer.parseInt(st.nextToken());
+
+			q.addFirst( addOne );
+			sb.append(q.pollLast()).append(" ");
+		}
+		System.out.println(sb);
+
+	}
+}
