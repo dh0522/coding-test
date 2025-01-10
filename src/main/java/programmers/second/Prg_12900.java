7744e77999c7906bbe90010927959faa45e60dcb
dh0522 <gong4857@naver.com>
2025-01-10 19:32:58 +0900

    2 x n 타일링 (Lv.2)

---
+++ src/main/java/programmers/second/Prg_12900.java
@@ -0,0 +1,21 @@
+package programmers.second;
+
+public class Prg_12900 {
+	public static void main(String[] args) {
+		Prg_12900 sol = new Prg_12900();
+
+		System.out.println(sol.solution(4));
+	}
+	private static int solution( int n ){
+		int[] dp = new int[n+1];
+
+		dp[1] = 1;
+		dp[2] = 2;
+		int res = 1;
+		for( int i = 3; i <= n; i ++ ){
+			dp[i] = (dp[i-1] + dp[i-2])%1000000007;
+		}
+
+		return dp[n];
+	}
+}
