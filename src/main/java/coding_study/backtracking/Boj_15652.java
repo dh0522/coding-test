b8e0a72c2a3541f6fb30e6844092faf169a7fcf7
dh0522 <gong4857@naver.com>
2025-02-01 11:50:55 +0900

    N과 M (4) (실버3)

---
+++ src/main/java/coding_study/backtracking/Boj_15652.java
@@ -0,0 +1,35 @@
+package coding_study.backtracking;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_15652 {
+	static StringBuilder sb = new StringBuilder();
+	static int[] arr;
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		arr = new int[m];
+
+		backTracking(1, n, m, 0);
+		System.out.println(sb);
+	}
+	private static void backTracking(int now, int n, int m, int depth){
+		if (depth == m){
+			for (int i: arr)
+				sb.append(i).append(" ");
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = now; i <= n ;i ++ ){
+			arr[depth] = i;
+			backTracking(i, n, m, depth+1);
+		}
+	}
+}
