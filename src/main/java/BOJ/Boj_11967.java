97f36d75abd263ea028258d7feee5e4e1851d363
dh0522 <gong4857@naver.com>
2025-10-31 20:34:30 +0900

    불켜기(골드 2)

--- src/main/java/BOJ/Boj_11967.java
+++ src/main/java/BOJ/Boj_11967.java
@@ -2,113 +2,132 @@ package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Boj_11967 {
-	static int n;
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	static ArrayList<Room>[][] arr;
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		n = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		arr = new ArrayList[n][n];
 
+		List<Room>[][] map = new List[n+1][n+1];
 
-		for ( int i =0; i <n ; i ++ ){
-			for (int j = 0; j < n; j ++ )
-				arr[i][j] = new ArrayList<>();
+		for (int i =1 ; i <= n; i++ ){
+			for (int j =1; j <= n; j++){
+				map[i][j] = new ArrayList<>();
+			}
 		}
 
-
-		for (int i=0; i <m ; i++ ){
+		while (m-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken())-1;
-			int y = Integer.parseInt(st.nextToken())-1;
-			int a = Integer.parseInt(st.nextToken())-1;
-			int b = Integer.parseInt(st.nextToken())-1;
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
-			arr[x][y].add( new Room(a,b) );
+			map[x][y].add(new Room(a,b));
 		}
 
-		bfs();
+		int answer = bfs(map, n);
 
+		System.out.println(answer);
 
 	}
-	private static void bfs() {
+	private static int bfs(List<Room>[][] map, int n){
 
-		Queue<Room> q = new LinkedList<>();
-		q.add( new Room(0,0) );
+		int[] dx = {1,0,-1,0};
+		int[] dy = {0,1,0,-1};
 
-		boolean[][] visited = new boolean[n][n];
-		visited[0][0] = true;
-		boolean[][] light = new boolean[n][n];
-		light[0][0] = true;
+		// 1. 불 킨 방
+		boolean[][] isLight = new boolean[n+1][n+1];
 
-		boolean[][] move = new boolean[n][n];
+		// 2. 방문해서 진짜로 불을 킨 배열
+		boolean[][] visited = new boolean[n+1][n+1];
+
+		// 3. 방문할 수 있는지
+		boolean[][] canVisited = new boolean[n+1][n+1];
+
 
 		int answer = 1;
 
-		while( !q.isEmpty() ){
+		Queue<Room> q = new LinkedList<>();
+		q.add(new Room(1,1));
+
+		canVisited[1][1] = true;
+		visited[1][1] = true;
+		isLight[1][1] = true;
+
+		while(!q.isEmpty()){
 			Room now = q.poll();
 
-			for ( int i =0; i < 4; i++ ){
-				int nx = now.x+ dx[i];
-				int ny = now.y+ dy[i];
+			int nowx = now.x;
+			int nowy = now.y;
 
-				if ( nx < 0 || ny < 0 || nx >=n || ny >= n )
+			// 1. 현재 위치에서 갈 수 있는곳 체크
+			for (int d = 0; d< 4; d++){
+				int nx = nowx + dx[d];
+				int ny = nowy + dy[d];
+
+				if (nx < 1 || ny < 1|| nx >= n+1 || ny >= n+1)
 					continue;
 
-				move[nx][ny] = true;
+				canVisited[nx][ny] = true;
 			}
 
-			for ( int i=0; i < arr[now.x][now.y].size(); i++ ){
-				Room temp = arr[now.x][now.y].get(i);
+			// 불을 킬 수 있는지 체크하고
+			// 불을 킬 수 있고 지금 방문 가능하면 Q에 넣는다.
+			for (int i = 0; i < map[nowx][nowy].size(); i++) {
+				Room poss = map[nowx][nowy].get(i);
 
-				//불이 꺼져있다면 -> 켜주기
-				if( !light[temp.x][temp.y] ){
-					light[temp.x][temp.y] = true;
+				if (!isLight[poss.x][poss.y] ) {
+					isLight[poss.x][poss.y] = true;
 					answer++;
 				}
 
-				if( move[temp.x][temp.y] ){
-					q.add( temp );
-					visited[temp.x][temp.y] = true;
+				if (canVisited[poss.x][poss.y] && !visited[poss.x][poss.y]){
+					visited[poss.x][poss.y] = true;
+					q.add(poss);
 				}
-			}
-			for ( int i=0; i < 4; i++ ){
-				int nx = now.x + dx[i];
-				int ny = now.y + dy[i];
-
-				if ( nx < 0 || ny < 0 || nx >=n || ny >= n )
-					continue;
-
-				if ( !light[nx][ny] || !move[nx][ny] || visited[nx][ny] )
-					continue;
-
-				visited[nx][ny] = true;
-				q.add( new Room(nx,ny) );
 
 			}
 
-		}
-		System.out.println(answer);
-		return;
+			for (int d = 0; d< 4; d++){
+				int nx = nowx + dx[d];
+				int ny = nowy + dy[d];
+
+				if (nx < 1 || ny < 1|| nx >= n+1 || ny >= n+1)
+					continue;
+
+				if (isLight[nx][ny] && !visited[nx][ny]){
+					visited[nx][ny] = true;
+					q.add(new Room(nx,ny));
+				}
+
+			}
 
 
-	}
-	static class Room {
-		int x,y;
-		public Room( int x , int y  ){
-			this.x = x;
-			this.y = y;
 		}
+		return answer;
+
 	}
 }
+
+class Room  {
+	int x;
+	int y;
+	public Room(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
+
+
+
